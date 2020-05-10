fde982b4239cd879e39e11187d82c4359f4b8e64
Exsayudy Manalu <exsam578@gmail.com>
2020-05-10 20:54:57 +0700

    Exsayudy Manalu
    Menu
    Update
    Gejala

---
+++ AplikasiAnakDesa/app/src/main/java/stud11418038/develops/aplikasianakdesa/batuk.java
@@ -0,0 +1,14 @@
+package stud11418016.develops.antisipasicovid19;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class batuk extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_batuk);
+    }
+}
