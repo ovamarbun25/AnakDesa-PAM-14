fde2bf3ddbafbd5ab751737c6f58518d6dd92def
Ova Marbun <ovaferdinanmarbun@gmail.com>
2020-06-24 15:10:13 +0700

    Ova Marbun

---
+++ AplikasiAnakDesa/app/src/main/java/stud11418038/develops/aplikasianakdesa/UI/Activities/Provinsi/Activity_Provinsi.java
@@ -0,0 +1,16 @@
+package stud11418038.develops.aplikasianakdesa.UI.Activities.Provinsi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import stud11418038.develops.aplikasianakdesa.R;
+
+public class Activity_Provinsi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__provinsi);
+    }
+}
--- AplikasiAnakDesa/app/src/main/java/stud11418038/develops/aplikasianakdesa/UI/Activities/menu.java
+++ AplikasiAnakDesa/app/src/main/java/stud11418038/develops/aplikasianakdesa/UI/Activities/menu.java
@@ -3,14 +3,27 @@ package stud11418038.develops.aplikasianakdesa.UI.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import stud11418038.develops.aplikasianakdesa.R;
 
-public class menu extends AppCompatActivity {
+public class menu extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        findViewById(R.id.updateCovid).setOnClickListener(this);
+        findViewById(R.id.tipsTrick).setOnClickListener(this);
+        findViewById(R.id.lokasiMedis).setOnClickListener(this);
+        findViewById(R.id.gejala).setOnClickListener(this);
+        findViewById(R.id.cekDiri).setOnClickListener(this);
+        findViewById(R.id.profile).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
